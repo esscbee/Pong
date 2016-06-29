4f4abf0b1c7a93b45726aaf54830ec0fa94146f6
esscbee <github@esscb.com>
2016-06-29 10:27:21 -0700

    initial working version

--- Pong/GameScene.swift
+++ Pong/GameScene.swift
@@ -10,6 +10,10 @@ import SpriteKit
 
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
+        self.physicsWorld.gravity = CGVectorMake(0, 0)
+        self.scaleMode = SKSceneScaleMode.ResizeFill
+        self.physicsBody = SKPhysicsBody(edgeLoopFromRect: frame)
+        print(frame)
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
@@ -17,9 +21,24 @@ class GameScene: SKScene {
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+        return
+        if let ball = childNodeWithName("//ball") {
+            if let pb = ball.physicsBody {
+                print(String(format:"%X", pb.categoryBitMask), " \(pb.velocity)")
+                if let pb2 = physicsBody {
+                    print(String(format:"%X", pb2.collisionBitMask), " ", frame)
+                }
+            }
+        }
     }
     
     override func didChangeSize(oldSize: CGSize) {
-        physicsBody = SKPhysicsBody(edgeLoopFromRect: frame)
+        self.physicsBody = SKPhysicsBody(edgeLoopFromRect: frame)
+        self.physicsBody?.friction = 0
+        self.physicsBody?.restitution = 1
+        
+        
+//        self.physicsBody?.friction = 0
+        print("didChangeSize \(frame), size: \(size), \(physicsBody)")
     }
 }
